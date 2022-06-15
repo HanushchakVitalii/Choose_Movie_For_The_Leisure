d98028b6d3bd379ee3b6f3c0da189b7c1bc6d0f7
hanushchak <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>
2022-06-15 17:59:23 +0300

    Initial commit

---
+++ app/src/main/java/com/aureliano_vitalii/choose_movie_for_the_leisure/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aureliano_vitalii.choose_movie_for_the_leisure.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aureliano_vitalii.choose_movie_for_the_leisure.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
